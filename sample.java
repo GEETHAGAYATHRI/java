37ac9da1ed3c1f6f6d946763f196633761488398
geetha gayathri <126499120+GEETHAGAYATHRI@users.noreply.github.com>
2023-12-12 16:01:50 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,5 @@
-class sample
-{
-public static void main(String[]args)
-{
-system.out.println("hello world")
-}
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
